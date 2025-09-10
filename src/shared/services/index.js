1a584a864447bcfe7caffbfe293b957f89d9206d
hernan_silvavera <hernan.silvavera@greencodesoftware.com>
2025-09-10 19:39:55 -0300

    commit all

--- src/shared/services/index.js
+++ src/shared/services/index.js
@@ -2,19 +2,10 @@ import axios from "axios";
 
 import appDataService from "../appData/service";
 
-/*axios.defaults.baseURL = import.meta.env.NODE_ENV === "development"
-    ? "https://localhost:44370/api/"
-    : "http://api.ventas.ladevi.greencodesoftware.com/api/";*/
-//axios.defaults.baseURL = "http://200.80.220.11:8097/api";
-
-console.log("NODE_ENV:", import.meta.env.NODE_ENV);
+console.log("MODE:", import.meta.env.MODE);
 console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
-axios.defaults.baseURL =
-  import.meta.env.VITE_API_URL ||
-  "http://api.ventas.ladevi.greencodesoftware.com/api/";
-
-console.log("Final axios baseURL:", axios.defaults.baseURL);
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 // Agregar interceptor para debug
 axios.interceptors.request.use(config => {
